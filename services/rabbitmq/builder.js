00861f18adbdcc0de7dbef5a1cb2709cfe3a49f3
Melvin Versluijs <melvin.versluijs@isaac.nl>
2022-11-04 10:13:08 +0100

    Update versions and use configs to determine instance type

--- services/rabbitmq/builder.js
+++ services/rabbitmq/builder.js
@@ -3,31 +3,47 @@
 module.exports = {
     name: 'rabbitmq',
     config: {
-        version: '3.9-management',
+        version: '3.11',
         supported: [
-            '3.8',
-            '3.8-management',
             '3.9',
-            '3.9-management',
+            '3.10',
+            '3.11',
         ],
+        patchesSupported: true,
         creds: {
             user: 'user',
             password: 'user'
         },
+        persist: false,
+        port: '5672',
         confSrc: __dirname,
-        healthcheck: 'rabbitmq-diagnostics -q ping'
+        healthcheck: 'rabbitmq-diagnostics -q ping',
+        
+        // Custom config.
+        management: true,
+        alpine: true,
     },
     parent: '_service',
     builder: (parent, config) => class LandoRabbitmq extends parent {
         constructor(id, options = {}) {
             options = {...config, ...options};
+
+            const image =  `rabbitmq:${options.version}${options.management ? '-management' : ''}${options.alpine ? '-alpine' : ''}`;
+            const ports = ['5672'];
+            if (options.management) {
+                ports.push('15672');
+            }
+            
+            const environment = {
+                RABBITMQ_DEFAULT_USER: options.creds.user,
+                RABBITMQ_DEFAULT_PASS: options.creds.password
+            };
+
             const rabbitmq = {
-                image: `rabbitmq:${options.version}-alpine`,
+                image,
                 command: 'rabbitmq-server',
-                environment: {
-                    RABBITMQ_DEFAULT_USER: options.creds.user,
-                    RABBITMQ_DEFAULT_PASS: options.creds.password
-                }
+                ports,
+                environment,
             };
 
             super(id, options, {services: {[options.name]: rabbitmq}});
